f11909f62e891ac8ee2e50eed381f705d9e5621c
peterparker1908 <54286100+peterparker1908@users.noreply.github.com>
2021-11-25 13:50:22 +0700

    Update EmployeeService.js

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EMPLOYEE_API_BASE_URL = "http://mysqlcrudbackendaws-env.eba-r3atrvec.ap-southeast-1.elasticbeanstalk.com/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://mysqlcrudbackendaws-env.eba-r3atrvec.ap-southeast-1.elasticbeanstalk.com/";
 
 class EmployeeService {
     getEmployees(){
